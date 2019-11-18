ac65704b3158d02e1551ad576993a346e6a64b5f
User <user@Users-MBP.home>
2019-11-18 00:03:41 -0500

    test footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Footer = (props) => (
-    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
-    </footer>
+const Footer = props => (
+  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
+    <p className="copyright">
+      &copy; Tabitha Sin | Made in{' '}
+      <span role="img" aria-label="statue of liberty">
+        🗽
+      </span>{' '}
+      with copious amounts of{' '}
+      <span role="img" aria-label="dumplings">
+        🥟
+      </span>{' '}
+      | Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
+    </p>
+  </footer>
 )
 
 Footer.propTypes = {
-    timeout: PropTypes.bool
+  timeout: PropTypes.bool,
 }
 
 export default Footer
