210d37e5f73cb4ae6b95e642d96d82e4897a269a
User <user@Users-MBP.home>
2019-11-18 00:30:49 -0500

    last touches

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,19 @@ import React from 'react'
 import aboutPic from '../images/aboutPic.JPG'
 import devBeatz from '../images/devBeatz-homepage.png'
 import projectify from '../images/projectify-homepage.png'
+import skillsImg from '../images/skillsImg2.jpg'
+import bsIcon from '../images/bootstrapIcon.svg'
+import css3Icon from '../images/css3Icon.svg'
+import html5Icon from '../images/html5Icon.svg'
+import jsIcon from '../images/javascriptIcon.svg'
+import reactIcon from '../images/reactIcon.svg'
+import pSQLicon from '../images/postgresIcon.svg'
+import scssIcon from '../images/scssIcon.svg'
+import reduxIcon from '../images/reduxIcon.svg'
+import expressJSicon from '../images/expressJSicon.svg'
+import gitIcon from '../images/gitIcon.svg'
+import nodeJsIcon from '../images/nodeJSwhite.svg'
+import '../assets/css/techIcons.css'
 
 class Main extends React.Component {
   render() {
@@ -33,34 +46,16 @@ class Main extends React.Component {
             <img src={aboutPic} alt="Woman standing in front of colorful brick backdrop" />
           </span>
           <p>
-            <h3>Short Version:</h3> I am a full stack web developer looking for
-              my next opportunity to create change. My aim is to create
-              aesthetically pleasing and fully functional websites that clients
-              enjoy. Whether on my own or collaborating with a team, I dedicate
-              myself to ensuring that a story has come to life. I believe in
-              creating products that benefit the world at large.
+            <h3>Short Version:</h3> I am a full stack web developer looking for my next opportunity to create change. My aim is to create aesthetically pleasing and fully functional websites that clients enjoy. Whether on my own or collaborating with a team, I dedicate myself to ensuring that a story has come to life. I believe in creating products that benefit the world at large.
           </p>
           <p>
             <h3>Long Version:</h3> In the life before this one, I used to head
               to Panera during my lunch breaks for their tomato soup, Frontega
-              sandwich, and free WiFi. My laptop opened to the latest JavaScript
-              problem presented through a free learning service, and my lunch
-              would stay uneaten as I tried to understand and solve the problem.
-              It was during these moments where it seemed my neurons electrified,
-              waking me from a doldrums I didn't know I was experiencing.
-              Satisfaction rushed through me when my code eventually passed.
-            <p />I no longer wanted to chase after random hours to learn coding,
-            and that was how I ended up in Dallas, Texas, attending a full
-            stack, web-immersive bootcamp. It was there in the sweltering summer
-            heat where I realized I wanted nothing more than to bridge my
-            passion for storytelling and advocacy by building web applications
-            for the world to see. Good work, progressive missions, should be
-            highlighted for everyone.
+              sandwich, and free WiFi. My laptop opened to the latest JavaScript problem presented through a free learning service, and my lunch would stay uneaten as I tried to understand and solve the problem. It was during these moments where it seemed my neurons electrified, waking me from a doldrums I didn't know I was experiencing. Satisfaction rushed through me when my code eventually passed.
+            <p />I no longer wanted to chase after random hours to learn coding, and that was how I ended up in Dallas, Texas, attending a full stack, web-immersive bootcamp. It was there in the sweltering summer heat where I realized I wanted nothing more than to bridge my passion for storytelling and advocacy by building web applications for the world to see. Good work, progressive missions, should be highlighted for everyone.
             <p />
             When I am not trying to think of ways to get people's works out in
-            the world, you can find me outlining and writing science
-            fiction/horror stories, making dumplings, or training for
-            powerlifting.
+            the world, you can find me outlining and writing science fiction/horror stories, making dumplings, or training for powerlifting.
           </p>
           {close}
         </article>
@@ -88,21 +83,11 @@ class Main extends React.Component {
             </a>
           </h3>
           <p>
-            DevBeatz is the marketplace for aspiring producers and vocal artists
-              to trade their talents with each other. Producers registered onto
-              the site in order to upload their beats while vocal artists could
-              peruse through the selection. Producers had a customized dashboard
-              that tracked how much revenue they received from each individual
-              track, which tracks were their most popular, and which beats they
-              downloaded. Clients could browse through multiple genres of beats,
-              and they had the option to either purchase a beat for exclusive
-              rights or not.
+            DevBeatz is the marketplace for aspiring producers and vocal artists to trade their talents with each other. Producers registered onto the site in order to upload their beats while vocal artists could peruse through the selection. Producers had a customized dashboard that tracked how much revenue they received from each individual track, which tracks were their most popular, and which beats they downloaded. Clients could browse through multiple genres of beats, and they had the option to either purchase a beat for exclusive rights or not.
             <br />
             <br />
             This was a group project at DevMountain that was pitched,
-            wireframed, and developed within 2 weeks. I was responsible for the
-            visual design of the entire website and learned and implemented
-            ChartJS for the producers' dashboard.
+            wireframed, and developed within 2 weeks. I was responsible for the visual design of the entire website and learned and implemented ChartJS for the producers' dashboard.
             <br />
             <br />
             <h4>Tech Used:</h4> HTML, CSS, Sass, AmazonS3, React JS, React
@@ -125,18 +110,15 @@ class Main extends React.Component {
           </h3>
           <p>
             Projectify is a gamification of your task management experience.
-            Rather than running through a regular to-do list, give yourself more
-            incentive to finish! In Projectify, you are an alien that has
+            Rather than running through a regular to-do list, give yourself more incentive to finish! In Projectify, you are an alien that has
             crash-landed on Earth and need money (points) to repair your
             spaceship in order to head back home.
             <br />
             <br />
-            This was my personal project at DevMountain. I pitched, wireframed,
-            and developed the entire site within 2 weeks.
+            This was my personal project at DevMountain. I pitched, wireframed, and developed the entire site within 2 weeks.
             <br />
             <br />
-            <h4>Tech Used:</h4> HTML, CSS, Sass, React JS, Redux,
-            React-Bootstrap, PostgreSQL, Node, Express, Stripe
+            <h4>Tech Used:</h4> HTML, CSS, Sass, React JS, Redux, React-Bootstrap, PostgreSQL, Node, Express, Stripe
           </p>
           {close}
         </article>
@@ -148,19 +130,89 @@ class Main extends React.Component {
             }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">About</h2>
+          <h2 className="major">Skills {"&"} Experience</h2>
           <span className="image main">
-            <img src={pic03} alt="" />
+            <img src={skillsImg} alt="" />
           </span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
-          </p>
+          <h3>Tech Stack</h3>
+          <div className="tech-icons">
+            <div>
+              <img src={html5Icon} alt="html icon" className="icon-resize" />
+              <span className="tech-skill">HTML</span>
+            </div>
+            <div>
+              <img src={css3Icon} alt="css icon" className="icon-resize" />
+              <span className="tech-skill">CSS</span>
+            </div>
+            <div>
+              <img src={jsIcon} alt="javascript icon" className="icon-resize" />
+              <span className="tech-skill">JS ES6</span>
+            </div>
+            <div>
+              <img src={reactIcon} alt="react icon" className="icon-resize" />{' '}
+              <span className="tech-skill">ReactJS</span>
+            </div>
+            <div>
+              <img src={reduxIcon} alt="redux icon" className="icon-resize" />{' '}
+              <span className="tech-skill">Redux</span>
+            </div>
+            <div className="break"></div>
+            <div>
+              <img
+                src={pSQLicon}
+                alt="postgresql icon"
+                className="icon-resize"
+              />{' '}
+              <span className="tech-skill">PostgreSQL</span>
+            </div>
+            <div>
+              <img src={bsIcon} alt="bootstrap icon" className="icon-resize" />{' '}
+              <span className="tech-skill">Bootstrap</span>
+            </div>
+            <div>
+              <img src={scssIcon} alt="sass icon" className="icon-resize" />{' '}
+              <span className="tech-skill">SCSS</span>
+            </div>
+            <div>
+              <img
+                src={expressJSicon}
+                alt="express js icon"
+                className="icon-resize"
+              />{' '}
+              <span className="tech-skill">Express</span>
+            </div>
+            <div>
+              <img
+                src={nodeJsIcon}
+                alt="node js icon"
+                className="icon-resize"
+              />{' '}
+              <span className="tech-skill">NodeJS</span>
+            </div>
+            <div>
+              <img src={gitIcon} alt="git icon" className="icon-resize" />{' '}
+              <span className="tech-skill">Git/Github</span>
+            </div>
+          </div>
+          <p />
+          <h3>Tech Education</h3>
+          <b>Modern React with Redux (Udemy)</b> Oct 2019-Present
+          <br />
+          React JS Curriculum
+          <br />
+          React, Redux, React Hooks
+          <br />
+          <br />
+          <b>DevMountain (Dallas, Texas)</b> Jun 2019-Sept 2019
+          <br />
+          Full Stack/SERN Web Immersive Curriculum
+          <br />
+          Github, HTML/CSS, Javascript, React, Redux, NodeJS, Express,
+          PostgreSQL
+          <p />
+          <h3>PROFESSIONAL EXPERIENCE</h3>
+          Click here for more in-depth information.
+        <p />
           {close}
         </article>
 
