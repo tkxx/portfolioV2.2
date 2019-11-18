628de5499d85686dd77a7ba38660799be983c6d8
User <user@Users-MBP.home>
2019-11-18 00:12:47 -0500

    add bio

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import pic01 from '../images/pic01.jpg'
+import aboutPic from '../images/aboutPic.JPG'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 
@@ -25,32 +25,42 @@ class Main extends React.Component {
           id="intro"
           className={`${this.props.article === 'intro' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
-          }`}
+            }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Intro</h2>
+          <h2 className="major">Hi, I'm Tabitha</h2>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={aboutPic} alt="Woman standing in front of colorful brick backdrop" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+            <h3>Short Version:</h3> I am a full stack web developer looking for
+              my next opportunity to create change. My aim is to create
+              aesthetically pleasing and fully functional websites that clients
+              enjoy. Whether on my own or collaborating with a team, I dedicate
+              myself to ensuring that a story has come to life. I believe in
+              creating products that benefit the world at large.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+            <h3>Long Version:</h3> In the life before this one, I used to head
+              to Panera during my lunch breaks for their tomato soup, Frontega
+              sandwich, and free WiFi. My laptop opened to the latest JavaScript
+              problem presented through a free learning service, and my lunch
+              would stay uneaten as I tried to understand and solve the problem.
+              It was during these moments where it seemed my neurons electrified,
+              waking me from a doldrums I didn't know I was experiencing.
+              Satisfaction rushed through me when my code eventually passed.
+            <p />I no longer wanted to chase after random hours to learn coding,
+            and that was how I ended up in Dallas, Texas, attending a full
+            stack, web-immersive bootcamp. It was there in the sweltering summer
+            heat where I realized I wanted nothing more than to bridge my
+            passion for storytelling and advocacy by building web applications
+            for the world to see. Good work, progressive missions, should be
+            highlighted for everyone.
+            <p />
+            When I am not trying to think of ways to get people's works out in
+            the world, you can find me outlining and writing science
+            fiction/horror stories, making dumplings, or training for
+            powerlifting.
           </p>
           {close}
         </article>
@@ -59,7 +69,7 @@ class Main extends React.Component {
           id="work"
           className={`${this.props.article === 'work' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
-          }`}
+            }`}
           style={{ display: 'none' }}
         >
           <h2 className="major">Work</h2>
@@ -88,7 +98,7 @@ class Main extends React.Component {
           id="about"
           className={`${this.props.article === 'about' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
-          }`}
+            }`}
           style={{ display: 'none' }}
         >
           <h2 className="major">About</h2>
@@ -111,7 +121,7 @@ class Main extends React.Component {
           id="contact"
           className={`${this.props.article === 'contact' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
-          }`}
+            }`}
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
